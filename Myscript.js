0b098d4fe08fe7961e5b060b4c34b0363911b7d2
abdisamad100 <abdisamadcade004@gmail.com>
2020-10-03 20:26:20 +0300

    commit all changes

--- Myscript.js
+++ Myscript.js
@@ -27,3 +27,13 @@ var submission = function() {
      "Ama"
     ];
 }
+let month = parseInt(document.getElementById("month").value);
+  let year = parseInt(document.getElementById("year").value);
+  let day = parseInt(document.getElementById("day").value);
+  let name =(document.getElementById("name").value);
+  let date0fbirth = new Date(year + "/" + month + "/" + day);
+  let results = date0fbirth.getDay();
+  let output = document.getElementById("output");
+  male = document.getElementById("male")
+  female = document.getElementById("female")
+  if (month =="" && year =="" && day =="" && name=="") {
